e3093370f3457b74f6eba7bef8469e30da59edf6
bd. <cantcode023@gmail.com>
2025-01-22 00:19:24 +0800

    fixed cant find name for attribute None , removed whitespaces for text_from_html

--- nurs/utils/text_processing.py
+++ nurs/utils/text_processing.py
@@ -3,7 +3,7 @@ from bs4.element import Comment
 import requests
 
 def tag_visible(element):
-    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+    if element.parent and element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
         return False
     if isinstance(element, Comment):
         return False
@@ -11,10 +11,10 @@ def tag_visible(element):
 
 def text_from_html(body):
     soup = BeautifulSoup(body, 'html.parser')
-    texts = soup.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)  
-    return u" ".join(t.strip() for t in visible_texts)
-
+    texts = soup.findAll(string=True)
+    visible_texts = filter(tag_visible, texts)
+    return ' '.join(' '.join(t.strip() for t in visible_texts).split())
+    
 def parse_text(url):
     html = requests.get(url).text
     return text_from_html(html)
