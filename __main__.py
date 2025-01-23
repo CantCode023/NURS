9441517ce844c92ee2a5d80f914538137956bab3
bd. <cantcode023@gmail.com>
2025-01-24 03:21:01 +0800

    removed code

--- __main__.py
+++ __main__.py
@@ -1,4 +1,1 @@
-from nurs import summarize, parse_text
-
-text = parse_text('https://freedium.cfd/https://medium.com/pythoneers/18-insanely-useful-python-automation-scripts-i-use-everyday-b3aeb7671ce9')
-print(summarize(text))
+# CLI coming soon
