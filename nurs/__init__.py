b70f22791a3f87608eca7c07788ef7902f9a5503
bd. <cantcode023@gmail.com>
2025-01-24 09:35:16 +0800

    referencing NURS

--- nurs/__init__.py
+++ nurs/__init__.py
@@ -10,4 +10,6 @@ from .summarizer import Summarizer
 
 from .encryption import Encryption
 
+from .main import NURS
+
 __all__ = ["load_config", "tag_visible", "text_from_html", "parse_text", "Encryption"]
