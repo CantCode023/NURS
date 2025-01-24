63c0661a6fb74e99523f5d93a45adbd23db9695c
bd. <cantcode023@gmail.com>
2025-01-24 09:34:20 +0800

    fixed get_id not working

--- nurs/main.py
+++ nurs/main.py
@@ -6,31 +6,51 @@ class NURS:
     def __init__(self, gemini_api_key, jb_app_token):
         self.gemini_api_key = gemini_api_key
         self.jb_app_token = jb_app_token
-        self.API_ENDPOINT = "https://ains.gov.my/api"
+        self.API_ENDPOINT = "https://ains-api.moe.gov.my/api"
         self.encryption = Encryption()
-        self.headers = {
+        self.option_headers = {
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.9',
             'Access-Control-Request-Headers': 'authorization',
             'Access-Control-Request-Method': 'GET',
+            'Cache-Control': 'no-cache',
             'Connection': 'keep-alive',
+            'Origin': 'https://ains.moe.gov.my',
+            'Pragma': 'no-cache',
             'Referer': 'https://ains.moe.gov.my/',
-            'User-Agent': 'Mozilla/5.0 (U; Linux x86_64; en-US) AppleWebKit/533.47 (KHTML, like Gecko) Chrome/49.0.3858.378 Safari/601',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0',
         }
         
-    def _update_headers(self):
-        new_header = self.headers
-        del new_header['Access-Control-Request-Headers']
-        del new_header['Access-Control-Request-Method']
-        new_header["Authorization"] = self.encryption.get_bearer_authorization(self.jb_app_token)
-        return new_header
-        
     def _request_api(self, url:str):
-        requests.options(url, headers=self.headers)
-        new_header = self._update_headers()
-        response = requests.get(url, headers=new_header).json()
+        parsed_url = url[1:] if url.startswith('/') else url
+        url = f"{self.API_ENDPOINT}/{parsed_url}"
+        
+        requests.options(url, headers=self.option_headers)
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Authorization': self.encryption.get_bearer_authorization(self.jb_app_token),
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Origin': 'https://ains.moe.gov.my',
+            'Pragma': 'no-cache',
+            'Referer': 'https://ains.moe.gov.my/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0',
+            'sec-ch-ua': '"Chromium";v="130", "Opera GX";v="115", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+        }
+        response = requests.get(url, headers=headers).json()
         return response
 
     def _get_id(self) -> int:
-        URL = f"{self.API_ENDPOINT}/users/me?populate=*"
+        URL = "/users/me?populate=*"
         response = self._request_api(URL)
         return response["id"]
 
