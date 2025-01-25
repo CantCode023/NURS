b9b77f9d6b8c03f312818eb925eb6c9309a1c157
bd. <cantcode023@gmail.com>
2025-01-26 00:58:54 +0800

    updated main.p.y

--- nurs/main.py
+++ nurs/main.py
@@ -2,6 +2,8 @@ import requests
 from typing import Optional
 from .utils.models import Nilam
 from .encryption import Encryption
+from .utils.logger import Logger
+logger = Logger()
 
 class NURS:
     def __init__(self, gemini_api_key, bearer):
@@ -27,7 +29,7 @@ class NURS:
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0'
         }
 
         headers = {
@@ -43,7 +45,7 @@ class NURS:
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0',
             'sec-ch-ua': '"Chromium";v="130", "Opera GX";v="115", "Not?A_Brand";v="99"',
             'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Windows"',
         }
@@ -63,11 +65,12 @@ class NURS:
                 'Sec-Fetch-Dest': 'empty',
                 'Sec-Fetch-Mode': 'cors',
                 'Sec-Fetch-Site': 'same-site',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0',
                 'sec-ch-ua': '"Chromium";v="130", "Opera GX";v="115", "Not?A_Brand";v="99"',
                 'sec-ch-ua-mobile': '?0',
                 'sec-ch-ua-platform': '"Windows"',
             }
+            print(data.json())
             json_data = {"data": data.json()}
             response = requests.request(method=method, url=url, headers=post_headers, json=json_data)
             return response.json()
@@ -76,13 +79,18 @@ class NURS:
 
     def _get_id(self) -> int:
         URL = "/users/me?populate=*"
+        logger.log("Requesting user ID...")
         response = self._request_api(URL, "GET")
         return response["id"]
 
     def upload(self, nilam:Nilam):
-        URL = "/nilam-records"
+        URL = "/nilam-records/submit"
+        logger.log("Fetching user ID...")
         nilam.user = self._get_id()
+        logger.success(f"User ID fetched: {nilam.user}")
+        logger.log("Encrypting data to generate provider...")
         nilam.provider = self.encryption.get_provider(nilam.get_provider_parameter())
+        logger.success(f"Successfully generated provider: {nilam.provider}")
 
         response = self._request_api(URL, method="POST", data=nilam)
         return response
