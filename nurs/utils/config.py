2ae89c1b2dadd366aecf85e817279d70841ff431
bd. <cantcode023@gmail.com>
2025-01-25 20:49:08 +0800

    added returning None if keys doesn't exist

--- nurs/utils/config.py
+++ nurs/utils/config.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-def load_config() -> dict[str, dict[str, str]]:
-    return {
-        "API_KEYS": {
-            "GEMINI_API_KEY": os.environ["GEMINI_API_KEY"],
-            "jb_app_token": os.environ["jb_app_token"],
+def load_config() -> dict[str, dict[str, str]] | None:
+    if "GEMINI_API_KEY" in os.environ:
+        return {
+            "API_KEYS": {
+                "GEMINI_API_KEY": os.environ["GEMINI_API_KEY"],
+                "jb_app_token": os.environ["jb_app_token"],
+            }
         }
-    }
+    return None
