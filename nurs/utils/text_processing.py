02b60360f165594cb8157137f9c3ea998bafd89e
bd. <cantcode023@gmail.com>
2025-01-26 00:58:08 +0800

    using headless browser to fetch page content because of cloudflare

--- nurs/utils/text_processing.py
+++ nurs/utils/text_processing.py
@@ -2,7 +2,21 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 from better_profanity import profanity
 from .exceptions import HasProfanity
-import requests
+from seleniumbase import SB
+from .logger import Logger
+logger = Logger()
+
+def get_page_source(url):
+    logger.log("Getting page content...")
+    with SB(uc=True, headless=True) as sb:
+        logger.warn("Captcha detected, bypassing...")
+        sb.activate_cdp_mode(url)
+        logger.success("Bypassed captcha")
+        while not sb.is_element_visible("body > div.fixed.bottom-4.left-4"):
+            sb.sleep(1)
+        html = sb.get_page_source()
+        logger.success("Successfully scraped page content")
+    return html
 
 def tag_visible(element):
     if element.parent and element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -10,19 +24,27 @@ def tag_visible(element):
     if isinstance(element, Comment):
         return False
     return True
+    
+def has_profanity(text) -> bool:
+    if "Javascript" in text and "cookie" in text:
+        return True
+    return profanity.contains_profanity(text)
 
 def text_from_html(body):
     soup = BeautifulSoup(body, 'html.parser')
+    
+    logger.info("Checking if text is safe...")
+    title = soup.find("title").text.split(" |")[0]
+    print(title)
+    filtered = has_profanity(title)
+    if filtered:
+        raise HasProfanity("Bad words found in title! Please try with another article.")
+        
     texts = soup.findAll(string=True)
     visible_texts = filter(tag_visible, texts)
     return ' '.join(' '.join(t.strip() for t in visible_texts).split())
     
-def has_profanity(text) -> bool:
-    return profanity.contains_profanity(text)
-    
 def parse_text(url) -> str:
-    html = requests.get(url).text
-    filter = has_profanity(html)
-    if filter:
-        raise HasProfanity("Bad words found in text! Please try with another article.")
+    logger.info("Getting text from URL...")
+    html = get_page_source(url)
     return text_from_html(html)
