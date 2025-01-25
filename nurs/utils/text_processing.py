1482c30777103b12a6197763e28730e09f2563a5
bd. <cantcode023@gmail.com>
2025-01-25 14:47:49 +0800

    added function to check profanity

--- nurs/utils/text_processing.py
+++ nurs/utils/text_processing.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
+from profanity_check import predict
+from .exceptions import HasProfanity
 import requests
 
 def tag_visible(element):
@@ -15,6 +17,12 @@ def text_from_html(body):
     visible_texts = filter(tag_visible, texts)
     return ' '.join(' '.join(t.strip() for t in visible_texts).split())
     
+def has_profanity(text):
+    return predict(text)
+    
 def parse_text(url):
     html = requests.get(url).text
+    filter = has_profanity(html)
+    if filter == [1]:
+        raise HasProfanity("Bad words found in text! Please try with another article.")
     return text_from_html(html)
